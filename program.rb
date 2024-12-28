0be97249b15d7bf1e3f6243de9a648bdf774f20f
Amit Natani <eng.amitnatani1993@gmail.com>
2024-12-28 18:16:16 +0530

    Added functionality for adding numbers separated with comma in string with test case

--- program.rb
+++ program.rb
@@ -2,5 +2,7 @@
 class Program
   def add(numbers)
     return 0 if numbers.nil? || numbers.empty?
+
+    numbers.split(",").map(&:to_i).sum
   end
 end
--- program_test.rb
+++ program_test.rb
@@ -10,4 +10,14 @@ class AddTest < Test::Unit::TestCase
     result = @program.add('')
     assert_equal 0, result
   end
+
+  def test_add_with_comma_delimeter
+    result = @program.add('1,2,3')
+    assert_equal 6, result
+  end
+
+  def test_add_with_comma_delimeter_and_spaces
+    result = @program.add('1, 2, 3')
+    assert_equal 6, result
+  end
 end
